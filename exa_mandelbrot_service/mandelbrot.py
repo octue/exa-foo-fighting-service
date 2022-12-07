2aadc6bd829e7d882e95b2628de901000e77f775
cortadocodes <cortado.codes@protonmail.com>
2022-12-07 13:34:27 +0000

    FIX: Remove Julia set functionality to avoid `c` value error

--- exa_mandelbrot_service/mandelbrot.py
+++ exa_mandelbrot_service/mandelbrot.py
@@ -1,7 +1,7 @@
 import numpy
 
 
-def generate_mandelbrot_set(width, height, x_range, y_range, number_of_iterations, c=None):
+def generate_mandelbrot_set(width, height, x_range, y_range, number_of_iterations):
     """Compute the heightmap for a mandelbrot or julia set fractal.
 
     Each iteration, the new value of a pixel is calculated as z = z^2 + c, where for a mandelbrot set (default)
@@ -28,22 +28,21 @@ def generate_mandelbrot_set(width, height, x_range, y_range, number_of_iteration
     # purpose here is just to provide a simple demo.
     for index, a in numpy.ndenumerate(x):
 
-        # Get default constant c (Mandelbrot sets use spatial coordinates as constants).
-        if c is None:
-            b = y[index]
-            c = (a, b)
+        # Get constant c (Mandelbrot sets use spatial coordinates as constants).
+        b = y[index]
+        c = (a, b)
 
         x_old = 0
         y_old = 0
-        k = 1
+        iteration = 1
 
-        while (k <= number_of_iterations) and ((x_old**2 + y_old**2) < 4):
+        while (iteration <= number_of_iterations) and (x_old**2 + y_old**2 < 4):
             x_new = x_old**2 - y_old**2 + c[0]
             y_new = 2 * x_old * y_old + c[1]
             x_old = x_new
             y_old = y_new
-            k += 1
+            iteration += 1
 
-        z[index] = k
+        z[index] = iteration
 
     return x, y, z
