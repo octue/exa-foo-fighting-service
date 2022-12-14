2f3ed701a189e00b4794b97afd0fb15fe1f5d38b
cortadocodes <cortado.codes@protonmail.com>
2022-12-14 12:44:56 +0000

    TST: Move irrelevant code outside of logging contexts

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,16 +11,16 @@ PACKAGE_ROOT = os.path.dirname(os.path.dirname(__file__))
 class TestApp(unittest.TestCase):
     def test_app_returns_early_if_duration_limit_reached(self):
         """Test that the app returns early if the maximum duration is reached."""
+        runner = Runner(
+            app_src=os.path.join(PACKAGE_ROOT, "exa_mandelbrot_service"),
+            twine=os.path.join(PACKAGE_ROOT, "twine.json"),
+            configuration_values={"duration_check_interval": 0.1},
+        )
+
         with self.assertLogs(
             logger=logging.getLogger("exa_mandelbrot_service"),
             level=logging.WARNING,
         ) as logging_context:
-            runner = Runner(
-                app_src=os.path.join(PACKAGE_ROOT, "exa_mandelbrot_service"),
-                twine=os.path.join(PACKAGE_ROOT, "twine.json"),
-                configuration_values={"duration_check_interval": 0.1},
-            )
-
             runner.run(
                 input_values={
                     "test_id": "33",
@@ -33,13 +33,13 @@ class TestApp(unittest.TestCase):
 
     def test_app_with_randomised_duration(self):
         """Test that the maximum duration can be randomised."""
-        with self.assertLogs(logger=logging.getLogger("app"), level=logging.INFO) as logging_context:
-            runner = Runner(
-                app_src=os.path.join(PACKAGE_ROOT, "exa_mandelbrot_service"),
-                twine=os.path.join(PACKAGE_ROOT, "twine.json"),
-                configuration_values={"duration_check_interval": 0.1},
-            )
+        runner = Runner(
+            app_src=os.path.join(PACKAGE_ROOT, "exa_mandelbrot_service"),
+            twine=os.path.join(PACKAGE_ROOT, "twine.json"),
+            configuration_values={"duration_check_interval": 0.1},
+        )
 
+        with self.assertLogs(logger=logging.getLogger("app"), level=logging.INFO) as logging_context:
             runner.run(
                 input_values={
                     "test_id": "33",
